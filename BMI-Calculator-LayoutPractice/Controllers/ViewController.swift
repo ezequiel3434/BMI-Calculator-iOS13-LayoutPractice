cac8dcb060dfe630124841c57dc7f3226b95355d
ezequiel3434 <ezequiel3434@gmail.com>
2020-09-06 20:20:28 -0300

    using UISliders to calculate the BMI

--- BMI-Calculator-LayoutPractice/Controllers/ViewController.swift
+++ BMI-Calculator-LayoutPractice/Controllers/ViewController.swift
@@ -10,11 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
 
-
+    @IBAction func heightSliderAction(_ sender: UISlider) {
+        //print(Double(sender.value).rounded(toPlaces: 2))
+        
+        heightLabel.text = String(format: "%.2f", sender.value) + "m"
+    }
+    
+    @IBAction func weightSliderAction(_ sender: UISlider) {
+        //print(Double(sender.value).rounded(toPlaces: 2))
+        print(Int(sender.value))
+        weightLabel.text = String(format: "%.0f", sender.value) + "Kg"
+    }
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        let bmi = weight  / pow(height, Float(2))
+        print(bmi)
+    }
 }
 
+extension Double {
+    /// Rounds the double to decimal places value
+    func rounded(toPlaces places:Int) -> Double {
+        let divisor = pow(10.0, Double(places))
+        return (self * divisor).rounded() / divisor
+    }
+}
